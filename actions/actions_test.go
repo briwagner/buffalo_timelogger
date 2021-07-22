9a333b30d0dbafaaebeba58663b9e5c41b79b3e6
Brian Wagner <briwagner@outlook.com>
2021-07-21 22:42:01 -0500

    add stub testing for pathFor helper

--- actions/actions_test.go
+++ actions/actions_test.go
@@ -3,8 +3,11 @@ package actions
 import (
 	"testing"
 
+	_ "github.com/gobuffalo/helpers"
+	"github.com/gobuffalo/helpers/paths"
 	"github.com/gobuffalo/packr/v2"
 	"github.com/gobuffalo/suite"
+	_ "github.com/gobuffalo/tags"
 )
 
 type ActionSuite struct {
@@ -21,4 +24,23 @@ func Test_ActionSuite(t *testing.T) {
 		Action: action,
 	}
 	suite.Run(t, as)
+}
+
+func Test_Helpers(t *testing.T) {
+	type Test struct {
+		input string
+		want  string
+	}
+
+	tests := []Test{
+		{input: "bosses", want: "/bosses"},
+		{input: "newBossesPath()", want: "/bosses/new"},
+	}
+
+	for _, tc := range tests {
+		p, _ := paths.PathFor(tc.input)
+		if p != tc.want {
+			t.Errorf("Got %s, want %s", p, tc.want)
+		}
+	}
 }
